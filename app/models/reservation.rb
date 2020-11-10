736c96a5d8977343c4b73b3fcdd50c164a0fecfa
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2020-11-10 13:44:21 -0400

    Rent-car project

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,14 @@
 class Reservation < ApplicationRecord
   belongs_to :client
   belongs_to :car
+  validate :data
+
+  def data
+    if date_start < Time.now
+      errors[:base] << 'Você não pode selecionar uma data menor que hoje'
+    elsif date_end < date_start
+      errors[:base] << 'Você não pode selecionar uma data menor que data de inicio'
+    end
+  end
+  
 end
